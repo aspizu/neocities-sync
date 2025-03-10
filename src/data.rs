987631e4407e8e769fd49f87530aa8c6d92d1ad7
aspizu <aspizu@protonmail.com>
2025-03-10 23:51:08 +0530

    fix: ensure config directory exists

--- src/data.rs
+++ src/data.rs
@@ -11,6 +11,7 @@ impl Data {
             directories::ProjectDirs::from("org", "aspizu", "neocities-sync").unwrap();
         let config_dir = dirs.config_dir().to_path_buf();
         let default_username_path = config_dir.join("default_username.txt");
+        fs::create_dir_all(&config_dir).unwrap();
         let default_username = fs::read_to_string(default_username_path).ok();
         Self { config_dir, default_username }
     }
